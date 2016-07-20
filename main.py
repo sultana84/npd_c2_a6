a5d7a0c7107d22641da1e694dc02df0c69bc26ec
sultana84 <sultanaisgneiss@gmail.com>
2016-07-20 23:56:53 +0000

    Assignment 6 files

---
+++ main.py
@@ -0,0 +1,8 @@
+import pkg.beatles.george
+from pkg.beatles import john
+
+# DO NOT EDIT BELOW THIS LINE
+print("George:", pkg.beatles.george.phrase)
+print("John:", john.phrase)
+print("Paul:", paul.phrase)
+print("Ringo:", ringo.phrase)
---
+++ pkg/beatles/paul.py
@@ -0,0 +1,7 @@
+from general import expressions
+ 
+# DO NOT EDIT BELOW THIS LINE
+phrase = expressions.polite("love {} in US, love {} in UK".format(
+    us.best,
+    uk.best,
+))
