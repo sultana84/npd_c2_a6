c81364d8a371de63e022cb361b706f4e45e5929d
sultana84 <sultanaisgneiss@gmail.com>
2016-07-23 11:26:49 +0000

    update files

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pkg.beatles.george
 from pkg.beatles import john
+from pkg.beatles import paul
+from pkg.beatles import ringo
+from pkg.countries.us import locations
+from pkg.countries.uk import locations
 
 # DO NOT EDIT BELOW THIS LINE
 print("George:", pkg.beatles.george.phrase)
--- pkg/beatles/george.py
+++ pkg/beatles/george.py
@@ -1,4 +1,7 @@
-
+from ..general.expressions import impolite
+from ..countries.us import locations as us_locations
+from ..countries.uk import locations as uk_locations
+from ..countries.uk import locations as uk_locations 
 # DO NOT EDIT BELOW THIS LINE
 phrase = impolite("I like {} in the US, and {} in the UK".format(
     us_locations.best,
